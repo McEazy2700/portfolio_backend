3f873da4723721acb248a74f9f6822f9491e0af9
Ezekiel Victor <102771161+McEazy2700@users.noreply.github.com>
2022-08-08 10:19:11 +0100

    added my doman

--- portfolio_backend/settings.py
+++ portfolio_backend/settings.py
@@ -140,7 +140,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CORS
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
-    'https://mceazy2700.github.io'
+    'https://mceazy2700.github.io',
+    'https://www.vice.codes/'
 ]
 
 # CLOUDINARY
